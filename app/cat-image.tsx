902d7f626b6977d3303520f5ee86c9c570937e40
manju314 <t198487ukk3x@gmail.com>
2026-02-01 10:26:49 +0900

    First commit

---
+++ app/cat-image.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useState } from "react";
+import { fetchImage } from "./fetch-image";
+import style from "./page.module.css";
+
+type CatImageProps = {
+    url: string;
+};
+
+export function CatImage({ url }: CatImageProps) {
+    const [imageUrl, setImageUrl] = useState(url);
+
+    const refreshImage = async () => {
+        setImageUrl(""); //初期化
+        const image = await fetchImage();
+        setImageUrl(image.url);
+    };
+
+    return (
+        <div className={style.page}>
+            <button onClick={refreshImage} className={style.button}>
+                他のにゃんこも見る
+            </button>
+            <div className={style.frame}>
+                {imageUrl && <img src={imageUrl} className={style.img} />}
+            </div>
+        </div>
+    );
+}
